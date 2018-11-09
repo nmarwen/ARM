515653b36681a251838ac82b3751e63b7dc810b2
nrennie <42338476+nrennie@users.noreply.github.com>
2018-11-09 09:31:10 +0000

    Update PassengerLoadFactorBookingsSimulation.R

--- PassengerLoadFactorBookingsSimulation.R
+++ PassengerLoadFactorBookingsSimulation.R
@@ -22,12 +22,13 @@ plfsimulation <- function(BH,mu,L,C,SLFs,segments1,rates1,segments2,rates2,p1,p2
     }
     else if ((sum(c(Y,M,K)) < SLFs[2]*C)){
       M[i] <- SLFs[2]*C - sum(c(Y,M,K))
+      #print(c("M",i))
     }
     else if ((sum(c(Y,M,K)) + Cust1[i]) < SLFs[1]*C){
-      Y[i] <- floor(Cust1[i]*p1)
+      Y[i] <- round(Cust1[i]*p1)
     }
     else if ((sum(c(Y,M,K)) < SLFs[1]*C)){
-      Y[i] <- floor((SLFs[1]*C - sum(c(Y,M,K)))*p1)
+      Y[i] <- round((SLFs[1]*C - sum(c(Y,M,K)))*p1)
     }
     #Cust2 arrivals
     if ((sum(c(Y,M,K)) + Cust2[i]) < SLFs[3]*C){
@@ -35,12 +36,14 @@ plfsimulation <- function(BH,mu,L,C,SLFs,segments1,rates1,segments2,rates2,p1,p2
     }
     else if ((sum(c(Y,M,K)) < SLFs[3]*C)){
       K[i] <- SLFs[3]*C - sum(c(Y,M,K))
+      #print(c("K",i))
     }
     else if ((sum(c(Y,M,K)) + Cust2[i]) < SLFs[2]*C){
-      M[i] <- M[i] + floor((Cust2[i])*p2)
+      M[i] <- M[i] + round((Cust2[i])*p2)
     }
     else if ((sum(c(Y,M,K)) < SLFs[2]*C)){
-      M[i] <- M[i] + floor((SLFs[2]*C - sum(c(Y,M,K)))*p2)
+      M[i] <- M[i] + round((SLFs[2]*C - sum(c(Y,M,K)))*p2)
+      #print(c("M",i))
     }
   }
   #Return the number of bookings made on each day
@@ -64,5 +67,4 @@ nplfsim <- function(n,BH,mu,L,C,SLFs,segments1,rates1,segments2,rates2,p1,p2,s,n
   filename <- paste(name, ".csv", sep="") 
   write.csv(km, filename, row.names=T)
 }
-
-nplfsim(n=500,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.6,0.25),segments1=c(10,10,10),rates1=c(20,30,40),segments2=c(15,10,5),rates2=c(5,35,50),p1=0.85,p2=0.65,s=12345,name="sim02")
+nplfsim(n=500,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.4,0.25),segments1=c(10,10,10),rates1=c(20,30,60),segments2=c(15,10,5),rates2=c(5,35,50),p1=0.85,p2=0.65,s=12345,name="sim02")
