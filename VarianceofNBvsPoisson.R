524e4f922e32e77d71d30cb197176bb09d0dea2d
nrennie <42338476+nrennie@users.noreply.github.com>
2018-11-20 12:12:21 +0000

    Update VarianceofNBvsPoisson.R

--- VarianceofNBvsPoisson.R
+++ VarianceofNBvsPoisson.R
@@ -24,6 +24,10 @@ lines(30:1,cumsum(aggBH(Cust1,30)+aggBH(Cust2,30)),type="s", lwd = 2, col = "bla
 legend("topleft", c("Customer Type 1 Arrivals","Customer Type 2 Arrivals","Total Arrivals"), col = c("red", "blue", "black"), fill = c("red", "blue", "black"), cex = 1.2)
 
 
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
+
 #Negative Binomial Arrivals
 #Homoegenous
 par(mfrow = c(1,2))
@@ -53,6 +57,10 @@ plot(30:1,cumsum(aggBH(Cust1NB,30)),type="s", lwd = 2, col = "red", xlim=c(30, 0
 lines(30:1,cumsum(aggBH(Cust2NB,30)),type="s", lwd = 2, col = "blue", xlim=c(30, 0))
 lines(30:1,cumsum(aggBH(Cust1NB,30)+aggBH(Cust2NB,30)),type="s", lwd = 2, col = "black", xlim=c(30, 0))
 legend("topleft", c("Customer Type 1 Arrivals","Customer Type 2 Arrivals","Total Arrivals"), col = c("red", "blue", "black"), fill = c("red", "blue", "black"), cex = 1.2)
+
+
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
 
 
 #Rewrite simulation code for different arrivals
@@ -127,6 +135,10 @@ NBnplfsim <- function(n,BH,mu,L,C,SLFs,segments1,sizes1,probs1,segments2,sizes2,
 NBnplfsim(n=500,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=c(2,3,4),probs1=c(0.5,0.5,0.5),segments2=c(15,10,5),c((1/3),3.5,10),probs2=c(0.5,0.5,0.5),p1=0.85,p2=0.65,s=12345,name="sim02NB")
 
 
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
+
 #import simulation data from csv
 simdat2 <- read.csv("sim02NB.csv", header=TRUE, stringsAsFactors = FALSE)
 #plot graphs and explore analysis
@@ -156,6 +168,10 @@ for (i in 2:k){
 par(mfrow = c(1,1))
 
 
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
+
 #Plot bookings and closure times for a single run with class closure times
 k <- NBplfsimulation(BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=c(2,3,4),probs1=c(0.5,0.5,0.5),segments2=c(15,10,5),sizes2=c((1/3),3.5,10),probs2=c(0.5,0.5,0.5),p1=0.85,p2=0.65)
 plot(BH:1,cumsum(k[,1]), type="s", lwd = 2, main="Customer Bookings", col = 2, xlim=c(BH, 0), ylim = c(0,200), xlab = "Booking Horizon (days)", ylab = "Customers", cex=1.2, cex.lab = 1.2, cex.axis = 1.2)
@@ -173,6 +189,11 @@ KbookingsNB <- simdat2[seq(3, nrow(simdat2), 3), ]
 TotbookingsNB <- YbookingsNB[,2:31] + MbookingsNB[,2:31] + KbookingsNB[,2:31] 
 lab <- rep("Tot",500)
 TotbookingsNB <- cbind(lab, TotbookingsNB)
+
+
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
 
 #Generate an outlier (single outlier, increase in mean and variance of type 1 customers only)
 NBnplfsim(n=1,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=c(3,4,5),probs1=c(0.4,0.4,0.4),segments2=c(15,10,5),sizes2=c((1/3),3.5,10),probs2=c(0.5,0.5,0.5),p1=0.85,p2=0.65,s=12345,name="sim02NBa")
@@ -222,4 +243,228 @@ outlierdet1(MbookingsNBa[,2:31],3)
 outlierdet1(KbookingsNBa[,2:31],4) 
 outlierdet1(TotbookingsNBa[,2:31],4) 
 
+
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
+
+#Rerun k-means clustering when variance is changing
+p <- c(0.9,0.7,0.5,0.3,0.1)
+fpoutputy <- list()
+genoutputy <- list()
+fpoutputm <- list()
+genoutputm <- list()
+fpoutputk <- list()
+genoutputk <- list()
+fpoutputtot <- list()
+genoutputtot <- list()
+for (i in 1:length(p)){
+  #create empty vectors to store results
+  fprates1y <- numeric(0)
+  genuine1y <- numeric(0)
+  fprates1m <- numeric(0)
+  genuine1m <- numeric(0)
+  fprates1k <- numeric(0)
+  genuine1k <- numeric(0)
+  fprates1tot <- numeric(0)
+  genuine1tot <- numeric(0)
+  #calculate sizes to keep mean constant
+  n1 <- (p[i]/(1-p[i]))*c(2,3,4)
+  n2 <- (p[i]/(1-p[i]))*c((1/3),3.5,10)
+  #run simulation to establish normal behaviour
+  NBnplfsim(n=500,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=n1,probs1=c(p[i],p[i],p[i]),segments2=c(15,10,5),sizes2=n2,probs2=c(p[i],p[i],p[i]),p1=0.85,p2=0.65,s=12345,name="VarTest1")
+  #create an outlier
+  NBnplfsim(n=1,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=n1+1,probs1=c(p[i],p[i],p[i]),segments2=c(15,10,5),sizes2=n2,probs2=c(p[i],p[i],p[i]),p1=0.85,p2=0.65,s=12345,name="VarTest1Out")
+  #read in data
+  VarTest1 <- read.csv("VarTest1.csv", header=TRUE, stringsAsFactors = FALSE)
+  YbookingsNBV <- simdat2[seq(1, nrow(VarTest1), 3), ]
+  MbookingsNBV <- simdat2[seq(2, nrow(VarTest1), 3), ]
+  KbookingsNBV <- simdat2[seq(3, nrow(VarTest1), 3), ]
+  TotbookingsNBV <- YbookingsNBV[,2:31] + MbookingsNBV[,2:31] + KbookingsNBV[,2:31] 
+  lab <- rep("Tot",500)
+  TotbookingsNBV <- cbind(lab, TotbookingsNBV)
+  #combine data and split into fare classes
+  VarTest1Out <- read.csv("VarTest1Out.csv", header=TRUE, stringsAsFactors = FALSE)
+  YbookingsNBaV <- rbind(YbookingsNBV,VarTest1Out[1,])
+  MbookingsNBaV <- rbind(MbookingsNBV,VarTest1Out[2,])
+  KbookingsNBaV <- rbind(KbookingsNBV,VarTest1Out[3,])
+  TotbookingsNBaV <- YbookingsNBaV[,2:31] + MbookingsNBaV[,2:31] + KbookingsNBaV[,2:31] 
+  lab <- rep("Tot",501)
+  TotbookingsNBaV <- cbind(lab, TotbookingsNBaV)
+  #run outlier detection and store resulst
+  ky <- outlierdet1(YbookingsNBaV[,2:31],5)
+  fprates1y <- c(fprates1y, fpratedists(ky,501))
+  genuine1y <- c(genuine1y, trueoutdet(ky,501))
+  km <- outlierdet1(MbookingsNBaV[,2:31],5)
+  fprates1m <- c(fprates1m, fpratedists(km,501))
+  genuine1m <- c(genuine1m, trueoutdet(km,501))
+  kk <- outlierdet1(KbookingsNBaV[,2:31],5)
+  fprates1k <- c(fprates1k, fpratedists(kk,501))
+  genuine1k <- c(genuine1k, trueoutdet(kk,501))
+  ktot <- outlierdet1(TotbookingsNBaV[,2:31],5)
+  fprates1tot <- c(fprates1tot, fpratedists(ktot,501))
+  genuine1tot <- c(genuine1tot, trueoutdet(ktot,501))
+  #store results for each p in a list
+  fpoutputy[[i]] <- fprates1y
+  genoutputy[[i]] <- genuine1y
+  fpoutputm[[i]] <- fprates1m
+  genoutputm[[i]] <- genuine1m
+  fpoutputk[[i]] <- fprates1k
+  genoutputk[[i]] <- genuine1k
+  fpoutputtot[[i]] <- fprates1tot
+  genoutputtot[[i]] <- genuine1tot
+}
+#separate data
+fpoutputy <- unlist(fpoutputy)
+genoutputy <- unlist(genoutputy)
+fpoutputm <- unlist(fpoutputm)
+genoutputm <- unlist(genoutputm)
+fpoutputk <- unlist(fpoutputk)
+genoutputk <- unlist(genoutputk)
+fpoutputtot <- unlist(fpoutputtot)
+genoutputtot <- unlist(genoutputtot)
+#plot results
+#Fare Class Y
+par(mfrow = c(2,2))
+d = data.frame(x = p, FPRate = fpoutputy)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class Y", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputy+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("bottomleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Repeat for other fare classes
+#Fare class M
+d = data.frame(x = p, FPRate = fpoutputm)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class M", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputm+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("top", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Fare Class K
+d = data.frame(x = p, FPRate = fpoutputk)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class K", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputk+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("topleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Total Bookings
+d = data.frame(x = p, FPRate = fpoutputtot)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Total Bookings", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputtot+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("bottomleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+
+
+#######################################################################################################################################################################################################
+#######################################################################################################################################################################################################
+
+
+#Rerun Euclidean distance outlier detection for changing variance
+#Rerun k-means clustering when variance is changing
+p <- c(0.9,0.7,0.5,0.3,0.1)
+fpoutputy2 <- list()
+genoutputy2 <- list()
+fpoutputm2 <- list()
+genoutputm2 <- list()
+fpoutputk2 <- list()
+genoutputk2 <- list()
+fpoutputtot2 <- list()
+genoutputtot2 <- list()
+for (i in 1:length(p)){
+  #create empty vectors to store results
+  fprates1y <- numeric(0)
+  genuine1y <- numeric(0)
+  fprates1m <- numeric(0)
+  genuine1m <- numeric(0)
+  fprates1k <- numeric(0)
+  genuine1k <- numeric(0)
+  fprates1tot <- numeric(0)
+  genuine1tot <- numeric(0)
+  #calculate sizes to keep mean constant
+  n1 <- (p[i]/(1-p[i]))*c(2,3,4)
+  n2 <- (p[i]/(1-p[i]))*c((1/3),3.5,10)
+  #run simulation to establish normal behaviour
+  NBnplfsim(n=500,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=n1,probs1=c(p[i],p[i],p[i]),segments2=c(15,10,5),sizes2=n2,probs2=c(p[i],p[i],p[i]),p1=0.85,p2=0.65,s=12345,name="VarTest1")
+  #create an outlier
+  NBnplfsim(n=1,BH=30,mu=180,L=0.01,C=200,SLFs=c(1,0.45,0.25),segments1=c(10,10,10),sizes1=n1+1,probs1=c(p[i],p[i],p[i]),segments2=c(15,10,5),sizes2=n2,probs2=c(p[i],p[i],p[i]),p1=0.85,p2=0.65,s=12345,name="VarTest1Out")
+  #read in data
+  VarTest1 <- read.csv("VarTest1.csv", header=TRUE, stringsAsFactors = FALSE)
+  YbookingsNBV <- simdat2[seq(1, nrow(VarTest1), 3), ]
+  MbookingsNBV <- simdat2[seq(2, nrow(VarTest1), 3), ]
+  KbookingsNBV <- simdat2[seq(3, nrow(VarTest1), 3), ]
+  TotbookingsNBV <- YbookingsNBV[,2:31] + MbookingsNBV[,2:31] + KbookingsNBV[,2:31] 
+  lab <- rep("Tot",500)
+  TotbookingsNBV <- cbind(lab, TotbookingsNBV)
+  #combine data and split into fare classes
+  VarTest1Out <- read.csv("VarTest1Out.csv", header=TRUE, stringsAsFactors = FALSE)
+  YbookingsNBaV <- rbind(YbookingsNBV,VarTest1Out[1,])
+  MbookingsNBaV <- rbind(MbookingsNBV,VarTest1Out[2,])
+  KbookingsNBaV <- rbind(KbookingsNBV,VarTest1Out[3,])
+  TotbookingsNBaV <- YbookingsNBaV[,2:31] + MbookingsNBaV[,2:31] + KbookingsNBaV[,2:31] 
+  lab <- rep("Tot",501)
+  TotbookingsNBaV <- cbind(lab, TotbookingsNBaV)
+  #run outlier detection and store resulst
+  ky <- eucdistoutlierdet(YbookingsNBaV[,2:31])
+  fprates1y <- c(fprates1y, fpratedists(ky,501))
+  genuine1y <- c(genuine1y, trueoutdet(ky,501))
+  km <- eucdistoutlierdet(MbookingsNBaV[,2:31])
+  fprates1m <- c(fprates1m, fpratedists(km,501))
+  genuine1m <- c(genuine1m, trueoutdet(km,501))
+  kk <- eucdistoutlierdet(KbookingsNBaV[,2:31])
+  fprates1k <- c(fprates1k, fpratedists(kk,501))
+  genuine1k <- c(genuine1k, trueoutdet(kk,501))
+  ktot <- eucdistoutlierdet(TotbookingsNBaV[,2:31])
+  fprates1tot <- c(fprates1tot, fpratedists(ktot,501))
+  genuine1tot <- c(genuine1tot, trueoutdet(ktot,501))
+  #store results for each p in a list
+  fpoutputy2[[i]] <- fprates1y
+  genoutputy2[[i]] <- genuine1y
+  fpoutputm2[[i]] <- fprates1m
+  genoutputm2[[i]] <- genuine1m
+  fpoutputk2[[i]] <- fprates1k
+  genoutputk2[[i]] <- genuine1k
+  fpoutputtot2[[i]] <- fprates1tot
+  genoutputtot2[[i]] <- genuine1tot
+}
+#separate data
+fpoutputy2 <- unlist(fpoutputy2)
+genoutputy2 <- unlist(genoutputy2)
+fpoutputm2 <- unlist(fpoutputm2)
+genoutputm2 <- unlist(genoutputm2)
+fpoutputk2 <- unlist(fpoutputk2)
+genoutputk2 <- unlist(genoutputk2)
+fpoutputtot2 <- unlist(fpoutputtot2)
+genoutputtot2 <- unlist(genoutputtot2)
+#plot results
+#Fare Class Y
+par(mfrow = c(2,2))
+d = data.frame(x = p, FPRate = fpoutputy2)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class Y", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputy2+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("bottomleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Repeat for other fare classes
+#Fare class M
+d = data.frame(x = p, FPRate = fpoutputm2)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class M", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputm2+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("top", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Fare Class K
+d = data.frame(x = p, FPRate = fpoutputk2)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Fare Class K", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputk2+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("topleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
+#Total Bookings
+d = data.frame(x = p, FPRate = fpoutputtot2)
+par(mar = c(5,5,2,5))
+with(d, plot(x, FPRate, type="l", col="red", xlab = "p", ylab="Number of False Positives", main = "Total Bookings", xlim = c(1,0), cex=1.2, cex.lab=1.2, cex.axis=1.2))
+par(new = T)
+with(d, plot(x, FPRate, col = genoutputtot2+1, axes=F, xlim = c(1,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
+legend("bottomleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c(1, 2))
 
