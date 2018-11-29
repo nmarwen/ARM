9354783a1e7abc6afa14a22d37c3944267d3d5cf
nrennie <42338476+nrennie@users.noreply.github.com>
2018-11-29 13:47:58 +0000

    Update EarlyDetection.R

--- EarlyDetection.R
+++ EarlyDetection.R
@@ -8,12 +8,14 @@ for (i in 1:11){
   fpratesEDY <- c(fpratesEDY, fpratedists(k,501))
   genuineEDY <- c(genuineEDY, trueoutdet(k,501))
 }
-par(mfrow = c(2,2))
-d = data.frame(x = 11:1, FPRate = fpratesEDY) 
-with(d, plot(x, rev(FPRate), type="l", col="black", xlab = "Booking Horizon", ylab="Number of False Positives", xlim = c(BH,0), main = "Fare Class Y", cex=1.2, cex.lab=1.2, cex.axis=1.2))
-par(new = T)
-with(d, plot(x, rev(FPRate), col = genuineEDY+1, axes=F, xlim = c(BH,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
-legend("topleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c("black", "red"))
+d <- data.frame(x = 11:1, FPRate = rev(fpratesEDY), gens = detornot(genuineEDY))
+p <- ggplot(data = d, aes(x = x)) + geom_line(size=1,aes(y = FPRate, linetype = "False Positives")) + 
+  geom_point(size=4,aes(y = FPRate,color=gens)) + scale_x_reverse(lim=c(30,0)) +
+  scale_colour_manual("", values = c("Outlier Not Detected" = "black", "Outlier Detected" = "red")) + 
+  labs(x = "Booking Horizon (DCPs before Departure)", y="Number of False Positives") + 
+  scale_y_continuous(limits=c(0,80)) +theme(axis.text=element_text(size=14),axis.title=element_text(size=14),legend.text=element_text(size=14),plot.background = element_rect(fill = "transparent", color = NA),legend.background = element_rect(color = NA,fill="transparent"),legend.box.background = element_rect(fill = "transparent",color=NA),legend.position=c(0,1),legend.justification=c(0,1),legend.title=element_blank(),legend.key = element_blank())
+p
+ggsave(p, filename = "EarlyDetY.png",  bg = "transparent")
 
 #Fare class M
 genuineEDM <- numeric(0)
@@ -24,11 +26,14 @@ for (i in 1:BH){
   fpratesEDM <- c(fpratesEDM, fpratedists(k,501))
   genuineEDM <- c(genuineEDM, trueoutdet(k,501))
 }
-d = data.frame(x = 30:1, FPRate = fpratesEDM) 
-with(d, plot(x, rev(FPRate), type="l", col="black", xlab = "Booking Horizon", ylab="Number of False Positives", xlim = c(BH,0), main = "Fare Class M", cex=1.2, cex.lab=1.2, cex.axis=1.2))
-par(new = T)
-with(d, plot(x, rev(FPRate), col = genuineEDM+1, axes=F, xlim = c(BH,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
-legend("topleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c("black", "red"))
+d <- data.frame(x = 30:1, FPRate = rev(fpratesEDM), gens = detornot(genuineEDM))
+p <- ggplot(data = d, aes(x = x)) + geom_line(size=1,aes(y = FPRate, linetype = "False Positives")) + 
+  geom_point(size=4,aes(y = FPRate,color=gens)) + scale_x_reverse(lim=c(30,0)) +
+  scale_colour_manual("", values = c("Outlier Not Detected" = "black", "Outlier Detected" = "red")) + 
+  labs(x = "Booking Horizon (DCPs before Departure)", y="Number of False Positives") + 
+  scale_y_continuous(limits=c(0,80)) +theme(axis.text=element_text(size=14),axis.title=element_text(size=14),legend.text=element_text(size=14),plot.background = element_rect(fill = "transparent", color = NA),legend.background = element_rect(color = NA,fill="transparent"),legend.box.background = element_rect(fill = "transparent",color=NA),legend.position=c(0,1),legend.justification=c(0,1),legend.title=element_blank(),legend.key = element_blank())
+p
+ggsave(p, filename = "EarlyDetM.png",  bg = "transparent")
 
 #Fare class K
 genuineEDK <- numeric(0)
@@ -39,11 +44,14 @@ for (i in 1:BH){
   fpratesEDK <- c(fpratesEDK, fpratedists(k,501))
   genuineEDK <- c(genuineEDK, trueoutdet(k,501))
 }
-d = data.frame(x = 30:1, FPRate = fpratesEDK) 
-with(d, plot(x, rev(FPRate), type="l", col="black", xlab = "Booking Horizon", ylab="Number of False Positives", xlim = c(BH,0), main = "Fare Class K", cex=1.2, cex.lab=1.2, cex.axis=1.2))
-par(new = T)
-with(d, plot(x, rev(FPRate), col = genuineEDK+1, axes=F, xlim = c(BH,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
-legend("bottomleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c("black", "red"))
+d <- data.frame(x = 30:1, FPRate = rev(fpratesEDK), gens = detornot(genuineEDK))
+p <- ggplot(data = d, aes(x = x)) + geom_line(size=1,aes(y = FPRate, linetype = "False Positives")) + 
+  geom_point(size=4,aes(y = FPRate,color=gens)) + scale_x_reverse(lim=c(30,0)) +
+  scale_colour_manual("", values = c("Outlier Not Detected" = "black", "Outlier Detected" = "red")) + 
+  labs(x = "Booking Horizon (DCPs before Departure)", y="Number of False Positives") + 
+  scale_y_continuous(limits=c(0,80)) +theme(axis.text=element_text(size=14),axis.title=element_text(size=14),legend.text=element_text(size=14),plot.background = element_rect(fill = "transparent", color = NA),legend.background = element_rect(color = NA,fill="transparent"),legend.box.background = element_rect(fill = "transparent",color=NA),legend.position=c(0,1),legend.justification=c(0,1),legend.title=element_blank(),legend.key = element_blank())
+p
+ggsave(p, filename = "EarlyDetK.png",  bg = "transparent")
 
 #Total Bookings
 genuineEDTot <- numeric(0)
@@ -54,9 +62,12 @@ for (i in 1:BH){
   fpratesEDTot <- c(fpratesEDTot, fpratedists(k,501))
   genuineEDTot <- c(genuineEDTot, trueoutdet(k,501))
 }
-d = data.frame(x = 30:1, FPRate = fpratesEDTot) 
-with(d, plot(x, rev(FPRate), type="l", col="black", xlab = "Booking Horizon", ylab="Number of False Positives", xlim = c(BH,0), main = "Total Bookings", cex=1.2, cex.lab=1.2, cex.axis=1.2))
-par(new = T)
-with(d, plot(x, rev(FPRate), col = genuineEDTot+1, axes=F, xlim = c(BH,0), pch=19, xlab=NA, ylab=NA, cex=1.2))
-legend("topleft", legend=c("Outlier Not Detected", "Outlier Detected"), pch=c(19,19), col=c("black", "red"))
+d <- data.frame(x = 30:1, FPRate = rev(fpratesEDTot), gens = detornot(genuineEDTot))
+p <- ggplot(data = d, aes(x = x)) + geom_line(size=1,aes(y = FPRate, linetype = "False Positives")) + 
+  geom_point(size=4,aes(y = FPRate,color=gens)) + scale_x_reverse(lim=c(30,0)) +
+  scale_colour_manual("", values = c("Outlier Not Detected" = "black", "Outlier Detected" = "red")) + 
+  labs(x = "Booking Horizon (DCPs before Departure)", y="Number of False Positives") + 
+  scale_y_continuous(limits=c(0,80)) +theme(axis.text=element_text(size=14),axis.title=element_text(size=14),legend.text=element_text(size=14),plot.background = element_rect(fill = "transparent", color = NA),legend.background = element_rect(color = NA,fill="transparent"),legend.box.background = element_rect(fill = "transparent",color=NA),legend.position=c(0,1),legend.justification=c(0,1),legend.title=element_blank(),legend.key = element_blank())
+p
+ggsave(p, filename = "EarlyDetTot.png",  bg = "transparent")
 
