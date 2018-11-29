375c5be6bc195fbac3e7b776298d59e603bea8a8
nrennie <42338476+nrennie@users.noreply.github.com>
2018-11-29 08:29:11 +0000

    Update CustomerArrivals.R

--- CustomerArrivals.R
+++ CustomerArrivals.R
@@ -44,15 +44,23 @@ aggBH <- function(data,BH){
 }
 aggBH(arrivals(30,0.01,180),30)
 
-
 #Generate two types of customer: type 1 and type 2
 Cust1 <- pharrivals(BH=30,L=0.01,segments=c(10,10,10),rates=c(20,30,40)) 
 Cust2 <- pharrivals(BH=30,L=0.01,segments=c(15,10,5),rates=c(5,35,50)) 
-#Save to files
+#Save to files and read in
 write.csv(Cust1, "Cust1.csv", row.names=F)
 write.csv(Cust2, "Cust2.csv", row.names=F)
+Cust1 <- read.csv("Cust1.csv", header=TRUE, stringsAsFactors = FALSE)
+Cust2 <- read.csv("Cust2.csv", header=TRUE, stringsAsFactors = FALSE)
+
 #Plot the customer arrivals of type 1 and type 2 customers
-plot(30:1,cumsum(aggBH(Cust1,30)),type="s", col = "red", xlim=c(30, 0), ylim = c(0,200), xlab = "Booking Horizon (days)", ylab = "Bookings")
-lines(30:1,cumsum(aggBH(Cust2,30)),type="s", col = "green", xlim=c(30, 0))
-lines(30:1,cumsum(aggBH(Cust1,30)+aggBH(Cust2,30)),type="s", col = "black", xlim=c(30, 0))
-legend("topleft", c("Customer Type 1 Arrivals","Customer Type 2 Arrivals","Total Arrivals"), col = c("red", "green", "black"), fill = c("red", "green", "black"))
+library(ggplot2)
+d <- data.frame(x = 30:1, y1 = cumsum(aggBH(Cust1[,1],30)), y2 = cumsum(aggBH(Cust2[,1],30)), y3 = cumsum(aggBH(Cust1[,1],30)+aggBH(Cust2[,1],30)))
+p <- ggplot(data = d, aes(x = x)) + geom_step(size=1,aes(y = y1, colour = "Type 1 (Business) Arrivals")) + 
+  geom_step(size=1,aes(y = y2, colour = "Type 2 (Tourist) Arrivals")) + geom_step(size=1,aes(y = y3, colour = "Total Customer Arrivals")) + 
+  scale_colour_manual("", values = c("Type 1 (Business) Arrivals"="red", "Type 2 (Tourist) Arrivals"="blue", "Total Customer Arrivals"="black")) + 
+  labs(x = "Booking Horizon (DCPs before Departure)") + scale_x_reverse(lim=c(30,0)) + scale_y_continuous("Cumulative Customer Arrivals", limits = c(0,200)) + 
+  theme(axis.text=element_text(size=14),axis.title=element_text(size=14),legend.text=element_text(size=14),plot.background = element_rect(fill = "transparent", color = NA),legend.background = element_rect(color = NA,fill="transparent"),legend.box.background = element_rect(fill = "transparent",color=NA),legend.position=c(0,1),legend.justification=c(0,1),legend.title=element_blank(),legend.key = element_blank())
+ggsave(p, filename = "CustomerArrivals.png",  bg = "transparent")
+
+
